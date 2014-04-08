4182f130c620bc4fff3d29bd2e59c133be79a470
Stuart Jones <stujo@users.noreply.github.com>
2014-04-08 15:50:26 -0700

    Update app.rb
    Standardizing Method Casing

--- app.rb
+++ app.rb
@@ -1,17 +1,16 @@
 require_relative 'lib/band.rb'
 
 band = Band.new('Gorillaz', 'Billy Connelly', '2000-03-23')
-	
+
 band.addRelease('Gorillaz', 2001, 1000001 )
 band.addRelease('Demon Days', 2005, 930430 )
 band.addRelease('Plastic Beach', 2010, 783211 )
 band.addRelease('The Fall', 2011, 8761232 )
 
-emailTitle = "Hi Fan! #{band} has a new release called #{band.latestRelease.name}"
-	
+emailTitle = "Hi Fan! #{band} has a new release called #{band.latest_release.name}"
+
 puts "#{band.name} has cumulative sales of of #{band.total_sales} units"
 
 puts "#{band.name} is managed by #{band.agent} and signed with us on #{band.date_signed}"
 
-puts "Currently we have #{Band.bandCount} bands signed to our label"
-
+puts "Currently we have #{Band.band_count} bands signed to our label"
